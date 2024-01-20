08f86ccc1b72b307a045a98fa380e2ed55678030
RC-002 <rohan.c@agens.email>
2024-01-20 18:21:33 +0530

    update: added postgres image config and init.sql files

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+-- Create the "zenskar" database
+CREATE DATABASE zenskar;
+
+-- Connect to the "zenskar" database
+\connect zenskar;
+
+-- Create the "customer" table in the "zenskar" database
+CREATE TABLE customer (
+    ID character varying(255) PRIMARY KEY,
+    name character varying(255) NOT NULL,
+    email character varying(255) UNIQUE NOT NULL
+);
