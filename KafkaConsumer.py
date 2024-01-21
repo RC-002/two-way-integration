e772b160972650522de966cdabc6a12d71d057e2
RC-002 <rohan.c@agens.email>
2024-01-21 16:04:51 +0530

    update: made changes to kafka listeners to send Customer data

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -1,19 +1,22 @@
 import json
 from kafka import KafkaConsumer, KafkaProducer
 
-kafka_server = ["localhost"]
 
-topic = "tests"
+class syncConsumer:
+    kafka_server = ["localhost"]
 
-consumer = KafkaConsumer(
-    bootstrap_servers=kafka_server,
-    value_deserializer=json.loads,
-    auto_offset_reset="latest",
-)
+    topic = "tests"
 
-consumer.subscribe(topic)
+    consumer = KafkaConsumer(
+        bootstrap_servers=kafka_server,
+        value_deserializer=json.loads,
+        auto_offset_reset="latest",
+    )
 
-while True:
-    data = next(consumer)
-    print(data)
-    print(data.value)
+    consumer.subscribe(topic)
+
+    def sync(self):
+        while True:
+            print("\n....................\n")
+            data = next(self.consumer)
+            print(data)
